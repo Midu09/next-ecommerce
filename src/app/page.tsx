aa2b86ca57c9ae91b2f275b6100a27a5a426e2f4
minhdung <dungdm@btc-studio.io>
2025-04-27 20:06:01 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 const HomePage = () => {
-  return (
-    <div className=''>HomePage</div>
-  )
-}
+  return <div className="">xin chao anh em</div>;
+};
 
-export default HomePage
+export default HomePage;
